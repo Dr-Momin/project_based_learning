749aa8c9b0f978ee67d6d6e9a560379fdf0a9393
Momin Khan <mmomin.bscsf19@iba-suk.edu.pk>
2022-12-05 01:15:31 +0500

    streak day 03 ==> day 02 progect again practiced..

--- 50_days_projects/Day-02_Progress-Steps/script.js
+++ 50_days_projects/Day-02_Progress-Steps/script.js
@@ -1,75 +1,68 @@
 
-
-
 const circles = document.querySelectorAll(".circle");
 
-const progress = document.getElementById("progress");
-const prev = document.getElementById("prev");
-const next = document.getElementById("next");
-
-let currentActive = 1;
+const progress = document.getElementById('progress');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
 
 
-
-
-
+let currentActve = 1;
 
 next.addEventListener("click", () => {
-   currentActive++;
 
-   if(currentActive > circles.length) {
-      currentActive = circles.length;
-   }
-
-   update();
-})
-
-
-prev.addEventListener("click", () =>{
-
-   currentActive--;
-
-   if(currentActive < 1){
-      currentActive = 1;
+   currentActve++;
+   
+   if(currentActve > circles.length){
+      currentActve = circles.length;
    }
 
    update();
 
-})
 
+});
+
+
+prev.addEventListener("click", () => {
+   
+   currentActve--;
+
+   if (currentActve < 1){
+      currentActve = 1;
+   }
+
+   update();
+
+})
 
 
 
 const update = () => {
+   console.log("in update func...")
 
    circles.forEach((circle, index) => {
 
-      if(index < currentActive) {
+      if(index < currentActve){
          circle.classList.add("active");
       }
-
       else{
          circle.classList.remove("active");
       }
-      
+
    });
 
 
    const actives = document.querySelectorAll(".active");
 
-   progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + "%";
+   progress.style.width = (actives.length - 1 ) / (circles.length -1 ) * 100 + "%";
 
-
-   if(currentActive === 1){
+   if(currentActve === 1){
       prev.disabled = true;
    }
-
-   else if(currentActive === circles.length){
+   else if(currentActve === circles.length){
       next.disabled = true;
+   }else{
+      next.disabled = false;
+      prev.disabled = false;
    }
 
-   else{
-      prev.disabled = false;
-      next.disabled = false;
-   }
 }
