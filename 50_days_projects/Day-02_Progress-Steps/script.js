76483d01b9bc901489f86dc2e8f42539d93994f9
Momin Khan <mmomin.bscsf19@iba-suk.edu.pk>
2022-12-10 17:30:21 +0500

    strek 01: day 02 project practice also done...

--- 50_days_projects/Day-02_Progress-Steps/script.js
+++ 50_days_projects/Day-02_Progress-Steps/script.js
@@ -1,68 +1,125 @@
 
+
+const btnNext = document.getElementById('next');
+const btnPrev = document.getElementById('prev');
+const progress = document.getElementById('progress');
 const circles = document.querySelectorAll(".circle");
 
-const progress = document.getElementById('progress');
-const prev = document.getElementById('prev');
-const next = document.getElementById('next');
+let countUpdate = 1;
 
 
-let currentActve = 1;
-
-next.addEventListener("click", () => {
-
-   currentActve++;
-   
-   if(currentActve > circles.length){
-      currentActve = circles.length;
+btnNext.addEventListener("click", () =>{
+  
+   countUpdate ++;
+   if(countUpdate > circles.length){
+      countUpdate = circles.length;
    }
 
    update();
-
-
 });
 
+btnPrev.addEventListener("click", () =>{
 
-prev.addEventListener("click", () => {
-   
-   currentActve--;
-
-   if (currentActve < 1){
-      currentActve = 1;
+   countUpdate--;
+   if (countUpdate < 1){
+      countUpdate = 1;
    }
 
    update();
-
 })
 
-
-
-const update = () => {
-   console.log("in update func...")
-
-   circles.forEach((circle, index) => {
-
-      if(index < currentActve){
+const update = () =>{
+   console.log("In update function......")
+   circles.forEach((circle, index) =>{
+      if(index < countUpdate){
          circle.classList.add("active");
       }
       else{
          circle.classList.remove("active");
       }
-
    });
-
 
    const actives = document.querySelectorAll(".active");
 
    progress.style.width = (actives.length - 1 ) / (circles.length -1 ) * 100 + "%";
 
-   if(currentActve === 1){
-      prev.disabled = true;
+   if(countUpdate === 1){
+      btnPrev.disabled = true;
    }
-   else if(currentActve === circles.length){
-      next.disabled = true;
-   }else{
-      next.disabled = false;
-      prev.disabled = false;
+   else if(countUpdate === circles.length){
+      btnNext.disabled = true;
+   }
+   else{
+      btnPrev.disabled = false;
+      btnNext.disabled = false;
    }
 
 }
+
+// const circles = document.querySelectorAll(".circle");
+
+// const progress = document.getElementById('progress');
+// const prev = document.getElementById('prev');
+// const next = document.getElementById('next');
+
+
+// let currentActve = 1;
+
+// next.addEventListener("click", () => {
+
+//    currentActve++;
+   
+//    if(currentActve > circles.length){
+//       currentActve = circles.length;
+//    }
+
+//    update();
+
+
+// });
+
+
+// prev.addEventListener("click", () => {
+   
+//    currentActve--;
+
+//    if (currentActve < 1){
+//       currentActve = 1;
+//    }
+
+//    update();
+
+// })
+
+
+
+// const update = () => {
+//    console.log("in update func...")
+
+//    circles.forEach((circle, index) => {
+
+//       if(index < currentActve){
+//          circle.classList.add("active");
+//       }
+//       else{
+//          circle.classList.remove("active");
+//       }
+
+//    });
+
+
+//    const actives = document.querySelectorAll(".active");
+
+//    progress.style.width = (actives.length - 1 ) / (circles.length -1 ) * 100 + "%";
+
+//    if(currentActve === 1){
+//       prev.disabled = true;
+//    }
+//    else if(currentActve === circles.length){
+//       next.disabled = true;
+//    }else{
+//       next.disabled = false;
+//       prev.disabled = false;
+//    }
+
+// }
