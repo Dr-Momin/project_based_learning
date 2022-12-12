69674726da06428933b2c5b29da498a80d112de9
Momin Khan <mmomin.bscsf19@iba-suk.edu.pk>
2022-12-12 22:00:46 +0500

    steak 03 - day 06 project practice done again...

--- 50_days_projects/Day-06_Scroll-animation/script.js
+++ 50_days_projects/Day-06_Scroll-animation/script.js
@@ -1,31 +1,78 @@
-console.log("Allooo")
+
+
+
 const boxes = document.querySelectorAll(".box");
-
-
-
 
 const checkboxes = () => {
 
    const triggerBottom = window.innerHeight / 7*3;
-   console.log(triggerBottom);
-
+   
    boxes.forEach((box) => {
       const boxTop = box.getBoundingClientRect().top;
 
-
-      if(boxTop < triggerBottom) {
+      if(boxTop < triggerBottom){
          box.classList.add("show");
       }
       else{
          box.classList.remove("show");
       }
 
-   });
-   
+
+   })
 
 }
 
 checkboxes();
 
-
 window.addEventListener("scroll", checkboxes);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("Allooo");
+// const boxes = document.querySelectorAll(".box");
+
+// const checkboxes = () => {
+
+//    const triggerBottom = window.innerHeight / 7*3;
+//    console.log(triggerBottom);
+
+//    boxes.forEach((box) => {
+//       const boxTop = box.getBoundingClientRect().top;
+
+
+//       if(boxTop < triggerBottom) {
+//          box.classList.add("show");
+//       }
+//       else{
+//          box.classList.remove("show");
+//       }
+
+//    });
+   
+
+// }
+
+// checkboxes();
+
+
+// window.addEventListener("scroll", checkboxes);
